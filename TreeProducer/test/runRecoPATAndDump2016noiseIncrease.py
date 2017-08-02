5131d4e3fa4f966d513bb0339dea1334206a31ea
amassiro <massironi.andrea@gmail.com>
2017-08-02 15:58:07 +0200

    fix

--- TreeProducer/test/runRecoPATAndDump2016noiseIncrease.py
+++ TreeProducer/test/runRecoPATAndDump2016noiseIncrease.py
@@ -249,11 +249,11 @@ process.ecalMultiFitUncalibRecHit.EEdigiCollection = cms.InputTag("ecalDigis","e
 process.ecalMultiFitUncalibRecHit.algoPSet.useLumiInfoRunHeader = cms.bool(False)
 
 #
-# activate floating pedestal
+# activate increased noise
 #  https://cmssdt.cern.ch/lxr/source/RecoLocalCalo/EcalRecProducers/python/ecalMultiFitUncalibRecHit_cfi.py
 #
-process.ecalMultiFitUncalibRecHit.algoPSet.dynamicPedestalsEB = cms.bool(True)
-process.ecalMultiFitUncalibRecHit.algoPSet.dynamicPedestalsEE = cms.bool(True)
+process.ecalMultiFitUncalibRecHit.algoPSet.addPedestalUncertaintyEB = cms.double(2.0)   # adc counts?
+process.ecalMultiFitUncalibRecHit.algoPSet.addPedestalUncertaintyEE = cms.double(4.0)   # adc counts?
 #
 #
 
